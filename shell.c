d69d446074e29d7a73146133c040f4ae1e19f5f0
Leo Leaipailote <lleaipailote@colgate.edu>
2022-11-29 04:29:05 -0500

    non-built-in functions

--- shell.c
+++ shell.c
@@ -6,6 +6,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <sys/wait.h>
 
 #define PROMPT "shell> "
 #define MAX_BACKGROUND 50
@@ -59,6 +61,45 @@ void free_tokens(char **tokens) {
     free(tokens); // then free the array
 }
 
+void builtInExit(char *command){
+    if(strcmp(command, "exit") == 0){
+        exit(EXIT_SUCCESS);
+    }
+}
+void builtInFg(char *command){
+}
+
+void nonBuiltIn(char **command ){
+    //find the index of either the NULL or the ampersand
+    int i = 0;
+    while(command[i]!= NULL && strcmp(command[i], "&") != 0){
+        i++;
+    }
+    char *last = command[i];    
+    int pid = fork();
+
+    char *args[i+1];
+    for(int j = 0; j < i; j++){
+        args[j] = command[j];
+    }
+    args[i] = NULL;
+   
+    if(pid < 0){
+        perror("fork failed");
+    }
+    else if(pid == 0){
+        if(execv(args[0], args) < 0){
+            perror("execv failed");
+        }
+    }
+    else{    
+        if(last == NULL){
+            waitpid(pid, NULL, 0);
+        }
+    }
+    
+}
+
 int main(int argc, char **argv) {    
     printf("%s", PROMPT);
     fflush(stdout);  // Display the prompt immediately
@@ -71,6 +112,16 @@ int main(int argc, char **argv) {
         }
         else {
             // TODO: run commands
+            if(strcmp(command[0], "exit") == 0){
+                builtInExit(command[0]);
+            }
+            else if(strcmp(command[0], "fg") == 0){
+            }
+            else{
+                nonBuiltIn(command);
+                
+                
+            }
         }
 
         free_tokens(command);
